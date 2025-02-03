fa5f55b8debbdce9a899b652a8989cfe779b3ee7
Rychillie <rychillie@rychillie.net>
2025-02-03 17:44:53 -0300

    Feat: Setup basic Layout

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,35 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
+    app.get { req -> EventLoopFuture<View> in
+        let context: [String: String] = [
+            "isActive": "Home"
+        ]
+
+        return req.view.render("Pages/index", context)
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("notifications") { req -> EventLoopFuture<View> in
+        let context: [String: String] = [
+            "isActive": "Bell"
+        ]
+
+        return req.view.render("Pages/notifications", context)
+    }
+
+    app.get("messages") { req -> EventLoopFuture<View> in
+        let context: [String: String] = [
+            "isActive": "Send"
+        ]
+
+        return req.view.render("Pages/messages", context)
+    }
+
+    app.get("bookmarks") { req -> EventLoopFuture<View> in
+        let context: [String: String] = [
+            "isActive": "Bookmark"
+        ]
+
+        return req.view.render("Pages/bookmarks", context)
     }
 }
