b4c409813f03b1d77db7a1e5dba9373c8e0e55db
Rychillie <rychillie@rychillie.net>
2025-02-24 11:21:37 -0300

    Fix: Rollback

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,9 +6,6 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    // configure the Leaf to use the new folder
-    app.leaf.configuration.rootDirectory = app.directory.workingDirectory + "Sources/App/Views"
-
     // register Leaf
     app.views.use(.leaf)
 
