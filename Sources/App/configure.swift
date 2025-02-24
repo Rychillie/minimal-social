3e3a01315a2cc31b2e98ee6becb409bfb7bc35a6
Rychillie <rychillie@rychillie.net>
2025-02-24 00:38:21 -0300

    Feat: Change folder structure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,21 @@
-import Leaf
+import LeafKit
 import Vapor
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    let pathToViews = URL(fileURLWithPath: #filePath).deletingLastPathComponent().appendingPathComponent("Views").relativePath
+    app.leaf.sources = LeafSources.singleSource(
+        NIOLeafFiles(
+            fileio: app.fileio,
+            limits: .default,
+            sandboxDirectory: pathToViews,
+            viewDirectory: pathToViews
+        )
+    )
 
     app.views.use(.leaf)
 
